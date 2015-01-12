2f3bcf010900c7e71b55c884f600571b4ef3563b
Maxwell Miller <maxwell.miller@live.com>
2015-01-11 19:51:07 -0500

    Fix filename not displaying on client

--- server/public/client/pi.js
+++ server/public/client/pi.js
@@ -1,9 +1,16 @@
-var ipmap = {};
-
 $(document).on('change', '.btn-file :file', function() {
     var input = $(this),
         numFiles = input.get(0).files ? input.get(0).files.length : 1,
         label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
 
     input.trigger('fileselect', [numFiles, label]);
+});
+
+$(document).ready(function() {
+    // When a file is selected, display it in the text box
+    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+        var input = $(this).parents('.input-group').find(':text');
+
+        input.val(label);
+    });
 });
