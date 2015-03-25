27251ca9f67e48af7ba9f747bbccf6043cb618c3
willcore <corlett.william@gmail.com>
2015-03-24 20:24:56 -0400

    Syntax Error - Window's New Line
    Previously 'known shippable' now tracked down and fixed. This should
    chew through everything you throw at it with no complaints. GCode can
    end a command with a typical Unix new line \n or a Windows Line Ending
    \r\n

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -172,7 +172,7 @@ def t_LETTER(t):
 
 # Define a rule so we can track line numbers
 def t_newline(t):
-    r'\n+'
+    r'\n+ | \r\n'
     t.lexer.lineno += len(t.value)
 
 # A string containing ignored characters (spaces and tabs)
